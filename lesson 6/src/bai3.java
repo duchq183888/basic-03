02f6f914cf8bcf1bf15db2cfcf983319a19600e3
duchq183888 <hqd162@gmail.com>
2019-10-14 14:22:29 +0700

    hw6

--- lesson 6/src/bai3.java
+++ lesson 6/src/bai3.java
@@ -14,10 +14,11 @@ public class bai3 {
 
     public static void main(String[] args) {
         int d = 0;
-        int n = 5;int seglen;
+        int n = 5;
+        int seglen;
         Scanner x = new Scanner(System.in);
         System.out.println("nhap do dai: ");
-        seglen=x.nextInt();
+        seglen = x.nextInt();
         int a[] = new int[5];
         System.out.println("nhap day a: ");
         for (int i = 0; i < 5; i++) {
@@ -25,18 +26,17 @@ public class bai3 {
             a[i] = x.nextInt();
 
         }
-        for(int i=0;i<n-1;i++){
-            for(int j=1+i;j<n;j++){
-                if(a[i]>a[j]){
-                    int k=a[i];
-                    a[i]=a[j];
-                    a[j]=k;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 1 + i; j < n; j++) {
+                if (a[i] > a[j]) {
+                    int k = a[i];
+                    a[i] = a[j];
+                    a[j] = k;
                 }
             }
         }
-        d=(a[a.length-1]-a[0])/seglen;
-                
-        
+        d = (a[a.length - 1] - a[0]) / seglen;
+
         System.out.println("seglen min: " + d);
 
     }
